eb88e00a7042f907d8d8506a60311a2baa74ab8a
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-09-01 11:31:55 +0530

    टेक्स्ट edited

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="card">
-        <h1>Friendship Day Surprise</h1>
+        <h1> Surprise</h1>
         <TextToSpeech />
       </div>
     </div>
